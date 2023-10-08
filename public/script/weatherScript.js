411f23eadaf27c8b9f31fe9d2868b2e41926d86f
NIKUNJ AGARWAL <133116644+nikunj95agarwal@users.noreply.github.com>
2023-10-08 13:08:37 +0530

    Add files via upload

--- public/script/weatherScript.js
+++ public/script/weatherScript.js
@@ -1,30 +1,35 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const cityName = document.getElementById('cityName');
-    const temperature = document.getElementById('temperature');
-    const humidity = document.getElementById('humidity');
-    const windSpeed = document.getElementById('windSpeed');
-    const searchInput = document.getElementById('searchInput');
-    const searchButton = document.getElementById('searchButton');
+const cityName = document.getElementById('cityName');
+const temperature = document.getElementById('temperature');
+const searchInput = document.getElementById('searchInput');
+const searchButton = document.getElementById('searchButton');
 
-    // Function to fetch weather data based on city name (you can use your API)
-    function fetchWeatherData(city) {
-        const apiKey = 'YOUR_API_KEY';
-        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${a3d179677347c576b3674b4cc3cba3cd}`)
-            .then(response => response.json())
-            .then(data => {
-                cityName.textContent = data.name;
-                temperature.textContent = `Temperature: ${data.main.temp}°C`;
-                humidity.textContent = `Humidity: ${data.main.humidity}%`;
-                windSpeed.textContent = `Wind Speed: ${data.wind.speed} km/h`;
-            })
-            .catch(error => console.error(error));
+const getInfo = async (event) => {
+  event.preventDefault();
+  if (searchInput.value === '') {
+    cityName.innerText = "WEATHER";
+  } else {
+    try {
+      let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchInput.value}&appid=a3d179677347c576b3674b4cc3cba3cd`;
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data);
+
+      if (data.cod === 200) {
+        temperature.innerText = `${data.main.temp}`;
+        cityName.innerText = data.name;
+      } else {
+        // Handle API errors
+        cityName.innerText = "WEATHER";
+        temperature.innerText = "";
+      }
+    } catch (error) {
+      // Handle fetch or parsing errors
+      console.error("Error:", error);
+      cityName.innerText = "WEATHER";
+      temperature.innerText = "";
     }
+  }
+}
 
-    // Event listener for the search button
-    searchButton.addEventListener('click', () => {
-        const city = searchInput.value.trim();
-        if (city !== '') {
-            fetchWeatherData(city);
-        }
-    });
-});
+// Event listener for the search button
+searchButton.addEventListener('click', getInfo);
